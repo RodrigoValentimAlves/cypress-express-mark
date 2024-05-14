f7bb8716da58876e7e36fa82c3454b6e97790bf0
Rodrigo Valentim <rodrigo.valentim.alves@outlook.com>
2024-05-14 16:27:13 -0300

    Adição do allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
@@ -6,8 +7,13 @@ module.exports = defineConfig({
     env: {
       apiUrl: 'http://localhost:3333',
     },
+
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
   },
 });
